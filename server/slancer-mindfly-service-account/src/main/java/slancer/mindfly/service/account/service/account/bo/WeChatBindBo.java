ea97a68c85e5c6b60547eaf23983c7a18148a136
xueyang <1607996261@qq.com>
2018-07-09 21:55:39 +0800

    fix CI
    (cherry picked from commit 66d6a4b)

--- server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/bo/WeChatBindBo.java
+++ server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/bo/WeChatBindBo.java
@@ -1,16 +1,10 @@
 package slancer.mindfly.service.account.service.account.bo;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
-@Getter
 @Setter
-@NoArgsConstructor
-@Accessors(chain = true)
+@Getter
 public class WeChatBindBO {
-
-	private String openId;
-
+    private String openId;
 }
