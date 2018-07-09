a87b11fc2e068b9dbf8074ccc0f9b0a3816edba1
xueyang <1607996261@qq.com>
2018-07-09 12:37:56 +0800

    fix ci

--- server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/controller/user/dto/WeChatLoginDTO.java
+++ server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/controller/user/dto/WeChatLoginDTO.java
@@ -3,10 +3,14 @@ package slancer.mindfly.service.account.controller.user.dto;
 import javax.validation.Valid;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
-@Setter
 @Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
 public class WeChatLoginDTO {
     private String code;
 }
