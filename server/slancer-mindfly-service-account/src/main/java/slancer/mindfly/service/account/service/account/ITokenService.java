b1317476ec52f62b6ad7d9600ba1005617a20aa3
xueyang <1607996261@qq.com>
2018-07-15 22:49:10 +0800

    fix test

--- server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/ITokenService.java
+++ server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/ITokenService.java
@@ -1,5 +1,8 @@
 package slancer.mindfly.service.account.service.account;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ITokenService<TP> {
     public String create(TP param);
 }
--- server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/UidTokenService.java
+++ server/slancer-mindfly-service-account/src/main/java/slancer/mindfly/service/account/service/account/UidTokenService.java
@@ -1,8 +1,10 @@
 package slancer.mindfly.service.account.service.account;
 
+import org.springframework.stereotype.Service;
 import slancer.mindfly.service.account.entity.UserEntity;
 import slancer.mindfly.service.account.service.account.bo.UserEntityToken;
 
+@Service
 public class UidTokenService implements ITokenService<UserEntityToken> {
     @Override
     public String create(UserEntityToken token) {
